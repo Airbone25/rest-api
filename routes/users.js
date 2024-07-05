cc13fd937b3ec9afceb2d2d2e50f43ee3816fb1c
Keshav Mehra <keshavmehra2005@gmail.com>
2024-07-05 21:25:36 +0530

    added delete request

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.get('/',(req,res)=>{
-    res.send("Hello REST api");
+router.get('/',async (req,res)=>{
+    try{
+        const users = await User.find();
+        res.json(users);
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
 })
 
 router.post('/',async (req,res)=>{
@@ -15,5 +20,21 @@ router.post('/',async (req,res)=>{
     const newUser = await user.save();
     res.status(201).json(newUser);
 })
+
+router.delete('/:id',getUser,async (req,res)=>{
+    await res.user.deleteOne();
+    res.json({message: 'User is removed'})
+})
+
+async function getUser(req,res,next){
+    let user;
+    user = await User.findById(req.params.id)
+    if(user==null){
+        res.status(404).json({message: 'Cannot Find User'})
+    }
+
+    res.user = user
+    next()
+}
 
 module.exports = router;
